a3f99594b29c58331db82fccc4d4368933678d6b
yangyan <yangyan@rimag.com.cn>
2017-08-22 17:32:43 +0800

    添加下拉刷新下拉加载

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-mui.init();
+mui.init({
+    pullRefresh : {
+        container:".home_list .mui-scroll-wrapper",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+        down : {
+            style:'circle',//必选，下拉刷新样式，目前支持原生5+ ‘circle’ 样式
+            color:'#2BD009', //可选，默认“#2BD009” 下拉刷新控件颜色
+            auto: true,//可选,默认false.首次加载自动上拉刷新一次
+            callback:function(){
+                mui(".home_list .mui-scroll-wrapper").pullRefresh().endPulldownToRefresh();
+            }
+        },up : {
+            auto:true,//可选,默认false.自动上拉加载一次
+            contentrefresh : "正在加载...",//可选，正在加载状态时，上拉加载控件上显示的标题内容
+            contentnomore:'没有更多数据了',//可选，请求完毕若没有更多数据时显示的提醒内容；
+            callback :function(){
+                mui(".home_list .mui-scroll-wrapper").pullRefresh().endPullupToRefresh(0);
+                //1 表示没有数据了  0 表示还有数据
+            }
+        }
+    }
+});
 
 mui('.mui-bar-tab').on('tap', 'a', function(e) {
     var targetTab = this.getAttribute('href');//获取目标子页的id
