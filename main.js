236d85fefca868aff102a9a3cfc9fbfc92aa5d31
yangyan <yangyan@rimag.com.cn>
2017-08-24 17:36:42 +0800

    编写我的页面

--- main.js
+++ main.js
@@ -20,13 +20,40 @@ mui.init({
     }
 });
 //初始化单页view
-mui('#app').view({
+var viewApi =mui('#app').view({
     defaultPage: '#setting'
 });
 //初始化单页的区域滚动
-mui('.mui-scroll-wrapper').scroll();
-//分享操作
-var shares = {};
+mui('.mui-scroll-wrapper').scroll({});
+
+var view = viewApi.view;
+(function($) {
+    //处理view的后退与webview后退
+    var oldBack = $.back;
+    $.back = function() {
+        if (viewApi.canBack()) { //如果view可以后退，则执行view的后退
+            viewApi.back();
+        } else { //执行webview后退
+            oldBack();
+        }
+    };
+    //监听页面切换事件方案1,通过view元素监听所有页面切换事件，目前提供pageBeforeShow|pageShow|pageBeforeBack|pageBack四种事件(before事件为动画开始前触发)
+    //第一个参数为事件名称，第二个参数为事件回调，其中e.detail.page为当前页面的html对象
+    view.addEventListener('pageBeforeShow', function(e) {
+        console.log(1);
+    });
+    view.addEventListener('pageShow', function(e) {
+        console.log(2);
+    });
+    view.addEventListener('pageBeforeBack', function(e) {
+        console.log(3);
+    });
+    view.addEventListener('pageBack', function(e) {
+        console.log(4);
+        //console.log(e.detail.page.id + ' back');
+    });
+})(mui);
+
 mui('.mui-bar-tab').on('tap', 'a', function(e) {
     var targetTab = this.getAttribute('href');//获取目标子页的id
     getTargetTab(targetTab);
@@ -36,12 +63,6 @@ function getTargetTab(url) {
 
 }
 
-mui('.mui-scroll-wrapper').scroll({
-    scrollY: true, //是否竖向滚动
-    scrollX: false, //是否横向滚动
-    startX: 0, //初始化时滚动至x
-    startY: 0, //初始化时滚动至y
-    indicators: true, //是否显示滚动条
-    deceleration:0.0006, //阻尼系数,系数越小滑动越灵敏
-    bounce: true //是否启用回弹
-});
+
+
+
